736d235e8d754f73abea73fddce2e951aee7ec1f
goswamiakshay <akshaygoswami765@gmail.com>
2022-12-30 13:53:55 +0530

    project created

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Food Scanner by Akshay and Abhishek</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login for User</div>
+  )
+}
+
+export default Login
